16129f1c74cf92511f5aa64b8839810021ed263c
Gregory Nietsky <gregnietsky@gmail.com>
2013-07-02 16:15:47 +0200

    Add functions for ungziping a buffer

--- src/zlib.c
+++ src/zlib.c
@@ -23,6 +23,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "include/dtsapp.h"
 
+static const unsigned char gzipMagicBytes[] = { 0x1f, 0x8b, 0x08, 0x00 };
+
 static void zobj_free(void *data) {
 	struct zobj *zdata = data;
 
@@ -47,7 +49,7 @@ extern struct zobj *zcompress(uint8_t *buff, uint16_t len, uint8_t level) {
 	if (!(ret->buff = malloc(ret->zlen))) {
 		return (NULL);
 	}
-	compress2(ret->buff, (uLongf*)&ret->zlen, buff, len, level);
+	compress2(ret->buff, (uLongf *)&ret->zlen, buff, len, level);
 
 	return (ret);
 }
@@ -63,4 +65,57 @@ extern void zuncompress(struct zobj *buff, uint8_t *obuff) {
 	}
 
 	uncompress(obuff, &olen, buff->buff, buff->zlen);
+}
+
+extern int is_gzip(uint8_t *buf, int buf_size) {
+	if (buf_size < 4) {
+		return 0;
+	}
+	if (memcmp(buf, gzipMagicBytes, 4)) {
+		return 0;
+	}
+	return 1;
+}
+
+extern uint8_t *gzinflatebuf(uint8_t *buf_in, int buf_size, uint32_t *len) {
+	z_stream zdat;
+	uint8_t *buf = NULL, *tmp;
+	int res;
+
+	zdat.opaque = NULL;
+	zdat.zalloc = NULL;
+	zdat.zfree = NULL;
+
+	zdat.next_in = buf_in;
+	zdat.avail_in = buf_size;
+	zdat.next_out = buf;
+	zdat.avail_out = 0;
+	zdat.total_out = 0;
+
+	if (inflateInit2(&zdat, 31)) {
+		return NULL;
+	}
+
+	do {
+		if (!(tmp = realloc(buf,zdat.total_out + (zdat.avail_in * 5) + 1))) {
+			res = Z_MEM_ERROR;
+			break;
+		} else {
+			buf = tmp;
+		}
+		buf[zdat.total_out] = '\0';
+		zdat.next_out = &buf[zdat.total_out];
+		zdat.avail_out += zdat.avail_in * 5;
+	} while ((res = inflate(&zdat, Z_NO_FLUSH)) == Z_OK);
+
+	if (res == Z_STREAM_END) {
+		buf = realloc(buf, zdat.total_out);
+		*len = zdat.total_out;
+	} else {
+		free(buf);
+		*len = 0;
+		buf = NULL;
+	}
+
+	return buf;
 }
